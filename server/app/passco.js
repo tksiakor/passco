95a8514e71c7654b3ce8c3e60af4cdeda69e2ff3
SenanuFC <senanu.fiam-coblavie@ashesi.edu.gh>
2014-11-13 14:31:19 +0000

    Nodejs capable of calling by topic and subject

--- server/app/passco.js
+++ server/app/passco.js
@@ -14,7 +14,8 @@ app.set('port', process.env.PORT || 3000);
 
 //passco Schema definition
 var passcoSchema = new mongoose.Schema({
-    year: [{year:Number, session: String}],
+    year: {type:Number},
+    session:{type:String},
     subject: {type: String},
     topic: {type: String},
     question: {type: String},
@@ -42,6 +43,20 @@ app.get('/getall', function(req, res){
 	
 	passco.find(function(err, passcos){
 		res.send(passcos);
+	})
+
+})
+
+//To retrieve by year
+
+//To retrieve by Topic
+app.get('/gettopic', function(req, res){
+	res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST");
+
+	passco.find({'topic': req.param("topic"), 'subject': req.param("subject")},function(err, passco1){
+		console.log(req.param("topic")+ " " + req.param("subject") )
+		res.send(passco1);
 	})
 
 })
