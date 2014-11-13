0ce6f47a1b2bdc0cc8c6e45dd7a90bbd45830a5f
SenanuFC <senanu.fiam-coblavie@ashesi.edu.gh>
2014-11-13 12:09:34 +0000

    Fixing headers

--- server/app/passco.js
+++ server/app/passco.js
@@ -36,6 +36,9 @@ var passco = mongoose.model('passco', passcoSchema, "passco" );
 
 //To retrieve all
 app.get('/getall', function(req, res){
+	res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST");
+  
 	
 	passco.find(function(err, passcos){
 		res.send(passcos);
